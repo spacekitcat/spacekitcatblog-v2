48ee033a5f9fc5fc0e59ab3ccbba9d437ad60532
Lisa Rae Burton <0lisa.burton@gmail.com>
2018-03-31 01:19:38 +0100

    updating for latest hackertextjs dep

--- spacekitcat/browserify/main.js
+++ spacekitcat/browserify/main.js
@@ -1,3 +1,3 @@
 var path = require('path');
 var fs = require('fs');
-var hackertext = require('../node_modules/hackertextjs/hackertext');
+var hackertext = require('../node_modules/hackertextjs/hackertext/dist');
