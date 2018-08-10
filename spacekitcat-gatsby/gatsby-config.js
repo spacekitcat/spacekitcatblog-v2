63806ccbb6f2446c0fde4f8f6beca3bda34055ab
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-08-10 21:38:48 +0100

    Initial commit of alt. gatsby based site.

---
+++ spacekitcat-gatsby/gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'SPACEKITCAT ',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+}
---
+++ spacekitcat-gatsby/static/cat-client-bootstrap.js
@@ -0,0 +1,12 @@
+var hacker_text_config =
+{
+  targets: [{
+  htmlId: "hackertext",
+  text: "SPACEKITAT",
+  renderer: {
+    strategy: "SinePhaseFrameRenderStrategy"
+  },
+  framerate: 1,
+  rows: 5
+  }]
+};
