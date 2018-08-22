f2b43a42b2ee6fb18a6b67129680f4bf9b99d605
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-08-22 10:07:22 +0100

    I feel numbery today.

--- spacekitcat-gatsby/src/layouts/index.jsx
+++ spacekitcat-gatsby/src/layouts/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import shuffle from 'lodash/shuffle';
 
 import Header from '../components/header';
 import TextGridBox from '../components/text-grid-box';
 import '../../sass/main.scss';
 
+const generateBoxValues = (boxCount, denomination) => {
+  const values = [];
+  for (let i = 0; i < boxCount; i += 1) {
+    values[i] = i * denomination;
+  }
+
+  return shuffle(values);
+};
+
 class Layout extends React.Component {
   constructor(props) {
     super(props);
+
+    const boxes = generateBoxValues(100, 1).map(item => (<TextGridBox text={item} />));
+    this.state = { boxes };
   }
 
   componentDidMount() {
@@ -50,36 +63,8 @@ class Layout extends React.Component {
         </div>
         <div className="gel-wrap">
           <Header siteTitle={this.props.data.site.siteMetadata.title} />
-
           <div className="questionboxwallrack">
-            <TextGridBox text="MEN CAN" />
-            <TextGridBox text="SABOTAGE ME ALL" />
-            <TextGridBox text="THEY WANT, IT" />
-            <TextGridBox text="WON'T FIX THEIR" />
-            <TextGridBox text="MEDIOCRITY." />
-            <TextGridBox text="CHOOSE" />
-            <TextGridBox text="GOOD." />
-            <TextGridBox text="CHOOSE" />
-            <TextGridBox text="COOPERATION." />
-            <TextGridBox text="CHOOSE" />
-            <TextGridBox text="DOING" />
-            <TextGridBox text="YOUR BIT FOR" />
-            <TextGridBox text="OUR FUTURE." />
-            <TextGridBox text="CHOOSE" />
-            <TextGridBox text="FOCUSING" />
-            <TextGridBox text="YOUR ENERGY ON" />
-            <TextGridBox text="SELF" />
-            <TextGridBox text="IMPROVEMENT." />
-            <TextGridBox text="CHOOSE" />
-            <TextGridBox text="EARNING THE" />
-            <TextGridBox text="RIGHT TO CALL" />
-            <TextGridBox text="YOURSELF AN" />
-            <TextGridBox text="ENGINEER." />
-            <TextGridBox text="LEARN TO" />
-            <TextGridBox text="OVERCOME YOUR" />
-            <TextGridBox text="MEDIOCRITY." />
-            <TextGridBox text={new Date().getUTCFullYear()} />
-            <TextGridBox text="GitHub" />
+            {this.state.boxes}
           </div>
         </div>
       </div>
