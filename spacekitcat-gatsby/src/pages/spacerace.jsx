9747565ec4bdcc155479b056ef69ec92744f7fde
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-10-28 22:52:57 +0000

    Renaming css classes for spacerace. Adding cool cut size ships

--- spacekitcat-gatsby/src/pages/spacerace.jsx
+++ spacekitcat-gatsby/src/pages/spacerace.jsx
@@ -13,37 +13,37 @@ const generateRandomPosition = () => {
 let stateVisibilityCounterRegister = true;
 const renderSnakes = (snakeState) => {
   let i = 0;
-  let statclass = 'snake__environment-stats';
+  let statclass = 'spacerace__environment-stats';
   if (!stateVisibilityCounterRegister) {
-    statclass += ' snake__environment-stats--alt';
+    statclass += ' spacerace__environment-stats--alt';
   }
   stateVisibilityCounterRegister = !stateVisibilityCounterRegister;
 
   const snakes = snakeState.map(state => (
-    <div className="snake__container" key={i}>
+    <div className="spacerace__container" key={i}>
       <div className={`${getPositionClass(state.position)} spacerace__sprite--number${i}`} key={i++} />
     </div>
   ));
   return (
-    <div className="snake__environment-wrapper">
-      <div className="snake__environment-wrongscreensize">
+    <div className="spacerace__environment-wrapper">
+      <div className="spacerace__environment-wrongscreensize">
         <h1>Your browser isn&#39;t wide enough for this jelly.</h1>
         <h3>The width should be at least 1280px, i.e. Desktop, extra wide tablet etc.</h3>
         <p>This is an experimental project prototype space for probability based games.</p>
         <p>The nature of prototyping allows for this to have very minimal thought towards interoperability.</p>
 
       </div>
-      <div className="snake__environment-container">
+      <div className="spacerace__environment-container">
         <div className={statclass}>
           {snakeState[0].position
           && `${sortBy(snakeState, o => o.position.x)[snakeState.length - 1].number} IS WINNING THE SPACE RACE`
         }
         </div>
-        <div className="snake__environmentstartlineleft">START</div>
-        <div className="snake__environmentfinishline">FINISH</div>
+        <div className="spacerace__environmentstartlineleft">START</div>
+        <div className="spacerace__environmentfinishline">FINISH</div>
         {snakes}
-        <div className="snake__environmentstartlineright">START</div>
-        <div className="snake__environmentfinishlinebottom">FINISH</div>
+        <div className="spacerace__environmentstartlineright">START</div>
+        <div className="spacerace__environmentfinishlinebottom">FINISH</div>
       </div>
     </div>
   );
