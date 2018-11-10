b7dd9015ca46c78cd8836abef8f82d635b83879d
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-11-10 00:21:46 +0000

    Ah ha ha ha ha ha

--- spacekitcat-react/src/stories/organisms/super-nav-bar/index.jsx
+++ spacekitcat-react/src/stories/organisms/super-nav-bar/index.jsx
@@ -5,7 +5,7 @@ import './_super-nav-bar.scss';
 const SuperNavBar = props => (
     <div className="supernavbar">
         <p className="supernavbar__text">
-        <span role="img" aria-label="Logo, a small rocket emoji">🚀</span> SPACEKITCAT
+        <span role="img" aria-label="Logo, a small rocket emoji"></span> SPACEKITCAT
         </p>
     </div>
 );
