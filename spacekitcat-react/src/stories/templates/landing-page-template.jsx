e6fdf125d51a7db105ada81507fc8df0c96e7e3b
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-11-10 02:28:33 +0000

    Adding section

--- spacekitcat-react/src/stories/templates/landing-page-template.jsx
+++ spacekitcat-react/src/stories/templates/landing-page-template.jsx
@@ -12,6 +12,7 @@ const LandingPageTemplate = props => (
                 <SuperNavBar />
                 <TopicPreview githuburl="https://github.com/spacekitcat/libz77" title="LZ77 compression algorothm exploration" text="Welcome. No one's home! Now scram--and don't come back!" />
                 <TopicPreview githuburl="https://github.com/spacekitcat/hackertextjs" title="Pure text Matrix esque animation experiments" text="I'm the biggest, baddest brute around, and don't you forget it." />
+                <TopicPreview githuburl="https://github.com/spacekitcat/example-lambda-project" title="AWS Lambda exploration" text="BAH! Nobody apologizes on my behalf! I'll be back in 3 rounds with a fiendish surprise for the player with my card!" />
             </header>
         </div>
     </div>);
