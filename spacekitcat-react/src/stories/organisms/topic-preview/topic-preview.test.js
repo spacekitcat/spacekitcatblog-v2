d63abad7348efb172180131b8d685fe7864e30cf
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-11-11 20:59:06 +0000

    Updating storybook snapshot. Not sure I really understand the value of snapshots, other than as a sort of regression guard, keeping an open mind about it. Added unit tests for the altstyle prop in topic preview.

--- spacekitcat-react/src/stories/organisms/topic-preview/topic-preview.test.js
+++ spacekitcat-react/src/stories/organisms/topic-preview/topic-preview.test.js
@@ -7,11 +7,25 @@ import TopicPreview from '.';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<TopicPreview />', () => {
-    const primaryExternalUrl = 'primaryExternalUrlValue';
-    const title = 'titleValue';
-    const abstractText = 'abstractTextValue';
+    let primaryExternalUrl 
+    let title; 
+    let abstractText;
+    let altstyle;
+    beforeEach(() => {
+        primaryExternalUrl = 'primaryExternalUrlValue';
+        title = 'titleValue';
+        abstractText = 'abstractTextValue';
+        altstyle = false;
+    });
+    
+    let component;
+    beforeAll(() => {
+        component = () => shallow(<TopicPreview primaryExternalUrl={primaryExternalUrl} title={title} abstractText={abstractText} altstyle={altstyle} />);
+    });
 
-    const component = () => shallow(<TopicPreview primaryExternalUrl={primaryExternalUrl} title={title} abstractText={abstractText} />);
+    it('should render the root element without the `alt` modifier class', () => {
+        expect(component().find('.topicpreview').hasClass('topicpreview--alt')).toBe(false);
+    });
 
     it('should render TopicPreview with the expected title', () => {
         expect(component().find('.topicpreview .topicpreview__title').text()).toBe('titleValue');
@@ -21,7 +35,21 @@ describe('<TopicPreview />', () => {
         expect(component().find('.topicpreview .topicpreview__title a').prop('href')).toBe(primaryExternalUrl);
     });
 
-    it('should render TopicPreview with the expected abstract', () => {
+    it('should render TopicPreview with the expected abstract text', () => {
         expect(component().find('.topicpreview .topicpreview__text').text()).toBe(abstractText);
     });
+
+    it('should render TopicPreview with the expected abstract text', () => {
+        expect(component().find('.topicpreview .topicpreview__text').text()).toBe(abstractText);
+    });
+
+    describe('when the altstyle prop is provided', () => {
+        beforeEach(() => {
+            altstyle = true;
+        });
+
+        it('should render the root element with the `alt` modifier class', () => {
+            expect(component().find('.topicpreview').hasClass('topicpreview--alt')).toBe(true);
+        });
+    })
 });
