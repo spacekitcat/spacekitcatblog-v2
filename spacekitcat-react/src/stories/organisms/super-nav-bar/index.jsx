f2fe226fdfa50212e056a37c0c1f4dae0ed1c763
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-11-15 23:40:27 +0000

    DiscoDavina

--- spacekitcat-react/src/stories/organisms/super-nav-bar/index.jsx
+++ spacekitcat-react/src/stories/organisms/super-nav-bar/index.jsx
@@ -1,14 +1,19 @@
-
 import React from 'react';
 import './_super-nav-bar.scss';
+import PlumberScarfFox from './discodavina.gif';
 
 const SuperNavBar = props => (
-    <div className="supernavbar">
-        <p className="supernavbar__text">
-        SPACEKITCAT
-        </p>
+  <div className="supernavbar">
+    <div className="supernavbar__spritepen">
+      <div className="supernavbar__sprite">
+        <img
+          src={PlumberScarfFox}
+          alt="A small, 8-bit style sprite. Imagine the head of Nintendo's Mario with two giant legs instead of a torso."
+        />
+      </div>
     </div>
+    <p className="supernavbar__text">SPACEKITCAT</p>
+  </div>
 );
 
 export default SuperNavBar;
-
